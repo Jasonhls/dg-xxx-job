c33103ed641641b0d4652db5810a2bf63945b071
huangna2019 <605343851@qq.com>
2019-07-23 14:33:18 +0800

    分布式job项目

---
+++ xxl-job-admin/src/main/java/com/xxl/job/admin/core/exception/XxlJobException.java
@@ -0,0 +1,14 @@
+package com.xxl.job.admin.core.exception;
+
+/**
+ * job Exception
+ */
+public class XxlJobException extends RuntimeException {
+
+    public XxlJobException() {
+    }
+    public XxlJobException(String message) {
+        super(message);
+    }
+
+}
