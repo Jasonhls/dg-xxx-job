a0b065f06be79509167e3589b40da55a2ad9500f
huangna2019 <605343851@qq.com>
2019-07-24 10:57:13 +0800

    core包增加配置类

--- xxl-job-admin/src/main/java/com/xxl/job/admin/XxlJobAdminApplication.java
+++ xxl-job-admin/src/main/java/com/xxl/job/admin/XxlJobAdminApplication.java
@@ -1,12 +1,13 @@
 package com.xxl.job.admin;
 
+import com.xxl.job.core.config.XxlJobConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * 应用入口 XxlJobAdminApplication
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = XxlJobConfig.class)
 public class XxlJobAdminApplication {
 
 	public static void main(String[] args) {
